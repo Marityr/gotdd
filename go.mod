9ae828d27995a5c359b6084c6dd29b20d00c7f69
Marityr <dmitriy.woronow@gmail.com>
2023-02-04 17:15:17 +0300

    helloworld

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Marityr/gotdd
+
+go 1.20
---
+++ hello/hello_test.go
@@ -0,0 +1,33 @@
+package hello
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+	t.Run("saying hellow to people", func(t *testing.T) {
+		got := Hello("Chris", "")
+		want := "Hello, Chris"
+
+		if got != want {
+			assertCorrectMessage(t, got, want)
+		}
+	})
+	t.Run("say, 'Hello, World' when an empty string is suppleid", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+
+		if got != want {
+			assertCorrectMessage(t, got, want)
+		}
+	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+}
