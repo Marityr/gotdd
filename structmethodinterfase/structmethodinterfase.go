2b49dad7facb6fab605ee0d632582940da0b7e73
Marityr <dmitriy.woronow@gmail.com>
2023-02-05 02:33:42 +0300

    structmethodinterfase

---
+++ structmethodinterfase/structmethodinterfase.go
@@ -0,0 +1,33 @@
+package structmethodinterfase
+
+import "math"
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func Perimetr(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Width)
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
+}
